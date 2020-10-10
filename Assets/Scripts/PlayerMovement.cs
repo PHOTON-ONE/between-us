6aa59bba71ccf2ed0f689d393d1256b7f5748299
13nicky <47931153+13nicky@users.noreply.github.com>
2020-10-10 20:14:09 +0200

    :tada: Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb2d;
+    public float speed;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector2 movement = new Vector2(horizontal, vertical);
+        rb2d.AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/TaskInteraction.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskInteraction : MonoBehaviour
+{
+    public Button btn;
+    public Canvas taskUI;
+    private Collider2D collid;
+    public string taskName;
+    private void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.tag == "Task")
+        {
+            collid = coll;
+            taskName = this.collid.name;
+            btn.onClick.AddListener(OpenTaskUI);  
+            btn.interactable = true;
+            Debug.Log("Enter");
+        }
+    }
+    private void OnTriggerExit2D(Collider2D coll)
+    {
+        if(coll.tag == "Task")
+        {
+            btn.interactable = false;
+            Debug.Log("Exit");
+        }
+    }
+
+    void OpenTaskUI()
+    {
+        if (!taskUI.enabled)
+        {
+            taskUI.enabled = true;
+        }
+        else if (taskUI.enabled)
+        {
+            taskUI.enabled = false;
+        }
+    }
+}
