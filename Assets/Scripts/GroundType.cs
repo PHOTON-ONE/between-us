ddf5e5a433d40577057ba10ee84a40963fcee960
13nicky <47931153+13nicky@users.noreply.github.com>
2020-10-12 21:37:02 +0200

    :sparkles: Added movement sounds

--- Assets/Scripts/GroundType.cs
+++ Assets/Scripts/GroundType.cs
@@ -1,20 +1,58 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Linq.Expressions;
+using UnityEngine;
 
 public class GroundType : MonoBehaviour
 {
     public PlayerMovement player;
-  //  [HideInInspector]
+    public float delay;
+    float timeCount;
+    int index = 1;
+    [HideInInspector]
     public string ground = "Default";
-    private void OnTriggerStay2D(Collider2D collision)
+    private void Start()
+    {
+        timeCount = delay;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         ground = collision.name;
-        if(player.isMoving == true)
-        {
-            Debug.Log("I'am moving!");
-        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         ground = "Default";
+    }
+    private void Update()
+    {
+        PlaySounds();
+    }
+    public void PlaySounds()
+    {
+        
+        if ((ground == "Default") && (player.isMoving))
+        {
+            if(!SetDelay())
+            {
+                if (index > 5)
+                {
+                    index = 1;
+                }
+                FindObjectOfType<AudioManager>().Play("Grass" + index);
+                index++;
+            }
+        }
+    }
+    public bool SetDelay()
+    {
+        if (timeCount < delay)
+        {
+            timeCount += Time.deltaTime;
+            return true;
+        }
+        else
+        {
+            timeCount = 0f;
+            return false;
+        }
     }
 }
