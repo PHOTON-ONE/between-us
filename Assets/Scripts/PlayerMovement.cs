170442ca2b40148c35edefdf6a2e23dc468fad49
13nicky <47931153+13nicky@users.noreply.github.com>
2020-10-12 12:37:02 +0200

    :sparkles: Added player graphic and movement animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,34 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb2d;
     public float speed;
+    public Animator animator;
+    public Transform rotate;
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        rotate = GetComponent<Transform>();
     }
     void FixedUpdate()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector2 movement = new Vector2(horizontal, vertical);
+        if (horizontal < 0)
+        {
+            rotate.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
+        }
+        else
+        {
+            rotate.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+        if ((vertical != 0) || (horizontal != 0))
+        {
+            animator.SetBool("IsWalking", true);
+        }
+        else 
+        {
+            animator.SetBool("IsWalking", false); 
+        }
         rb2d.AddForce(movement * speed * Time.fixedDeltaTime);
     }
 }
